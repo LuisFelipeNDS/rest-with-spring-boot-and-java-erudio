0506387e9848ab2416fdbaf3538eafc12e553b25
Felipe <Felipesss.2014@gmail.com>
2023-08-03 22:10:08 -0300

    Exception

--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
@@ -4,27 +4,46 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.erudio.exceptions.UnsupportedMathOperationException;
+
 @RestController
 public class MathController {
 	
 	private final String template = "Hello, %s!";
 	
-	@RequestMapping("/sum/{numberOne}/{numberTwo}") //@PathVariable recuperar dados da url
-	public String sum(@PathVariable(value = "numberOne") String numberOne, 
+	@RequestMapping("/sum/{numberOne}/{numberTwo}") //@PathVariable recuperar dados da URL
+	public Double sum(@PathVariable(value = "numberOne") String numberOne, 
 					  @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
 		
 		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {		
-			 throw new Exception("");
+			 throw new UnsupportedMathOperationException("Nos passe um valor numerico valido");
 		}
 		
 		return convertToDouble(numberOne) + convertToDouble(numberTwo);
 	}
 
-	private String convertToDouble(String strNumber) {
+	private Double convertToDouble(String strNumber) {
+		
+		if(strNumber == null) {
+			return 0D;
+		}
+		
+		String number = strNumber.replaceAll(",", ".");
+		
+		if(isNumeric(number)) {
+			return Double.parseDouble(number);
+		}
+		
 		return null;
 	}
 
-	private boolean isNumeric(String number) {
-		return false;
+	private boolean isNumeric(String strNumber) {
+		
+		if(strNumber == null) {
+			return false;
+		}
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+		
 	}
 }
